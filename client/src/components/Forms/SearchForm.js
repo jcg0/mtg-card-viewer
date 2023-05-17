84bc4b42e782f0d72eb943306c65383e72c59ca6
jcg0 <jg.awsprod@gmail.com>
2023-05-17 19:10:26 -0400

    created component for the search input

---
+++ client/src/components/Forms/SearchForm.js
@@ -0,0 +1,15 @@
+const SearchForm = () => {
+  return (
+    <form>
+      <div>
+        <div>
+          <label>Search</label>
+          <input type="text" />
+        </div>
+        <button type="submit">Search</button>
+      </div>
+    </form>
+  );
+};
+
+export default SearchForm;
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import SearchForm from "../Forms/SearchForm";
 
 const Header = () => {
   return (
     <nav>
-      <div>Login</div>
-      <div>Signup</div>
+      <div>
+        <SearchForm />
+      </div>
+      <section>
+        <div>
+          <Link to="/home">
+            <p>Home</p>
+          </Link>
+        </div>
+        <div>
+          <Link to="/login">
+            <p>Login</p>
+          </Link>
+        </div>
+        <div>
+          <Link to="/signup">
+            <p>Signup</p>
+          </Link>
+        </div>
+      </section>
     </nav>
   );
 };
